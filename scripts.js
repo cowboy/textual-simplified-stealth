acab70d12b6722f5ef77f6bc2844947974b75e79
Ben Alman <cowboy@rj3.net>
2012-07-24 11:27:08 -0400

    Adding license/copyright header to source code.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*
+ * Simplified Stealth (Textual)
+ * http://benalman.com/
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ * http://benalman.com/about/license/
+ */
+
+// Nothing yet!
